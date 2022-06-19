e60ce3edcf25d80dca3475391abe3d46a4a663cd
xin.jin <xin.jin@smartnews.com>
2022-06-20 00:25:27 +0800

    feat:add 7

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,11 +44,9 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Written by <strong>{author.name} </strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+          <a href={`https://twitter.com/${social?.twitter || ``}`}>Twitter</a>
         </p>
       )}
     </div>
