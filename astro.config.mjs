5ac89202cf55a474a6d782be1b55c655a6687cd2
xin.jin <jinxin479@gmail.com>
2023-05-11 21:13:17 +0800

    feat:change node v to 16

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
